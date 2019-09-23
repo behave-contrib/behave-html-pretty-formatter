aca23e3889c8bee9c37d27c0a05e8cb5f31a04ac
Petr Schindler <pschindl@redhat.com>
2019-09-23 13:38:55 +0200

    Basic functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+Html formatter for Behave.
+
+You can find information about behave on <https://behave.readthedocs.io/en/latest/>
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="behave-html-formatter",
+    version="0.0.5",
+    author="Petr Schindler",
+    author_email="pschindl@redhat.com",
+    description="HTML formatter for Behave",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/schidlo/behave-html-formatter",
+    packages=setuptools.find_packages(),
+    install_requires=['behave'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
