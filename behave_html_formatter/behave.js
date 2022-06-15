29aa3e4728a2f25f0a9d9ae6e83f5338d4af90df
Filip Pokryvka <fpokryvk@redhat.com>
2022-06-15 13:55:35 +0200

    Fix wrong minification of javascript
    After minification, there is semicolon added after the function name, 
    which is syntax error (at least in Firefox ESR). Moving brace line up 
    fixes this issue.

--- behave_html_formatter/behave.js
+++ behave_html_formatter/behave.js
@@ -1,28 +1,24 @@
-function Collapsible_toggle(id)
-{
+function Collapsible_toggle(id) {
     var elem = document.getElementById(id);
     elem.style.display = (elem.style.display == 'none' ? 'block' : 'none');
     return false;
 }
 
-function Collapsible_expandAll(className)
-{
+function Collapsible_expandAll(className) {
     var elems = document.getElementsByClassName(className);
     for (var i=0; i < elems.length; i++) {
         elems[i].style.display = 'block';
     }
 }
 
-function Collapsible_collapseAll(className)
-{
+function Collapsible_collapseAll(className) {
     var elems = document.getElementsByClassName(className);
     for (var i=0; i < elems.length; i++) {
         elems[i].style.display = 'none';
     }
 }
 
-function Collapsible_expandAllFailed()
-{
+function Collapsible_expandAllFailed() {
     var elems = document.getElementsByClassName('failed');
     for (var i=0; i < elems.length; i++) {
         var elem = elems[i];
