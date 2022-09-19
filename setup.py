a8186b7443fc86f9a39b038608fe322ddb63b856
Michal Odehnal <modehnal@redhat.com>
2022-09-19 11:38:23 +0200

    setup file innitial commit

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import setuptools
+
+def read_file(filename):
+    """
+    Return the content of a file.
+    """
+
+    with open(filename, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+setuptools.setup(
+    name="behave-html-pretty-formatter",
+    version="0.1",
+    author="Michal Odehnal",
+    author_email="modehnal@redhat.com",
+    description="HTML Pretty formatter for Behave",
+    url="https://github.com/behave-contrib/behave-html-pretty-formatter",
+    packages=setuptools.find_packages(),
+    install_requires=["behave"],
+    include_package_data=True,
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    long_description=read_file("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/behave-contrib/behave-html-formatter",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Testing :: BDD",
+    ],
+)
