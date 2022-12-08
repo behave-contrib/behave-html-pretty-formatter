6ab34140c77b07809231cc8acf6a0fec9a13f6de
Filip Pokryvka <fpokryvk@redhat.com>
2022-12-08 12:33:17 +0100

    Version increse to 1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_file(filename):
 
 setuptools.setup(
     name="behave-html-pretty-formatter",
-    version="1.1",
+    version="1.2",
     author="Michal Odehnal",
     author_email="modehnal@redhat.com",
     description="Pretty HTML Formatter for Behave",
