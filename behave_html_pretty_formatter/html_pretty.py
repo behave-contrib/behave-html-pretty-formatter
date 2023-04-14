7dc3944e51e419309f2e5a68e852235434fc08f7
Luca <luca.macavero@dorbit.space>
2023-04-14 10:38:40 +0200

    fix indentation

--- behave_html_pretty_formatter/html_pretty.py
+++ behave_html_pretty_formatter/html_pretty.py
@@ -499,8 +499,6 @@ class Scenario:
 
                 div(f"Scenario duration: {self.duration:.2f}s", cls="scenario-duration")
 
-        
-
         with div(
             cls=f"scenario-capsule {self.status.name}",
             id=f"f{self.feature.counter}-s{self.counter}-c",
@@ -508,7 +506,9 @@ class Scenario:
             # add scenario description as "commentary":
             scenario_description = "\n".join(self._scenario.description)
             if scenario_description:
-                pre(f"{scenario_description}", cls="step-capsule commentary margin-top")
+                pre(
+                    f"{scenario_description}", cls="step-capsule description margin-top"
+                )
 
             steps = self.steps
             if self.pseudo_steps:
