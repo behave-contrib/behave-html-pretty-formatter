8f1a87a5895a916e6f790b302b0a299efc729934
Michal Odehnal <modehnal@redhat.com>
2023-05-09 09:59:55 +0200

    Renaming variable to be more fitting. Adding high contrast and summary to hashes

--- behave_html_pretty_formatter/behave.js
+++ behave_html_pretty_formatter/behave.js
@@ -1,10 +1,15 @@
+// Handler that will call correct handlers based on what is triggered from the hash.
 document.addEventListener("DOMContentLoaded", function () {
-    for (var i = 0; i < embed_ids.length; i++) {
-        console.log("On load trying to toggle: " + embed_ids[i])
-        if (embed_ids[i] == "HC") {
+    for (var i = 0; i < hash_uuid_list.length; i++) {
+        console.log("On load trying to toggle: " + hash_uuid_list[i])
+        if (hash_uuid_list[i] == "high_contrast") {
+            // Trigger the high contrast.
             toggle_contrast(onload = true)
+        } else if (hash_uuid_list[i] == "summary") {
+            collapsible_summary("feature-summary-container", onload = true)
         } else {
-            onload_expander(embed_ids[i]);
+            // Triggering expand/collapse of embeds.
+            onload_expander(hash_uuid_list[i]);
         }
     }
 });
@@ -17,13 +22,13 @@ var toggle_non_empty_string = "#toggle="
 var current_url = new URL(document.URL);
 
 // Keeping the list of all toggled embeds.
-var embed_ids = new Array();
+var hash_uuid_list = new Array();
 if (current_url.hash.includes(toggle_non_empty_string)) {
     list_of_hashes = current_url.hash.replace(toggle_non_empty_string, "").split(",");
     for (var i = 0; i < list_of_hashes.length; i++) {
-        embed_ids.push(list_of_hashes[i])
+        hash_uuid_list.push(list_of_hashes[i])
     }
-    console.log("Starting ID list: " + embed_ids.toString())
+    console.log("Starting ID list: " + hash_uuid_list.toString())
 
 } else {
     // Add the default string to the url to prevent reloading on hash change.
@@ -36,10 +41,10 @@ if (current_url.hash.includes(toggle_non_empty_string)) {
 function generate_hash() {
     console.log("generate_hash")
 
-    if (embed_ids.length == 0) {
+    if (hash_uuid_list.length == 0) {
         current_url.hash = toggle_default_string
     } else {
-        current_url.hash = toggle_non_empty_string + embed_ids.toString()
+        current_url.hash = toggle_non_empty_string + hash_uuid_list.toString()
     }
     var new_url = current_url.href;
     document.location.href = new_url;
@@ -50,39 +55,41 @@ function toggle_hash_to_url(id) {
     console.log("toggle_hash_to_url: " + id.toString())
 
     // Check if the toggle has to go in or out of the hash.
-    console.log("Current contents of the list: " + embed_ids.toString())
-    console.log("Includes the item? " + embed_ids.includes(id.toString()))
+    console.log("Current contents of the list: " + hash_uuid_list.toString())
+    console.log("Includes the item? " + hash_uuid_list.includes(id.toString()))
 
-    if (embed_ids.includes(id.toString())) {
+    if (hash_uuid_list.includes(id.toString())) {
         console.log("Removing: " + id.toString())
-        for (var i = 0; i < embed_ids.length; i++) {
-            if (embed_ids[i] === id.toString()) {
-                embed_ids.splice(i, 1);
+        for (var i = 0; i < hash_uuid_list.length; i++) {
+            if (hash_uuid_list[i] === id.toString()) {
+                hash_uuid_list.splice(i, 1);
                 i--;
             }
         }
     } else {
         console.log("Adding: " + id)
 
-        embed_ids.push(id.toString())
+        hash_uuid_list.push(id.toString())
     }
     // Set new url.
     generate_hash()
 }
 
-
+// Making sure the container and content is changed.
 function onload_expander(id) {
+    // Making sure the arrow is pointing at the correct direction.
     var embed_container_id = "embed_container_" + id;
     var parent = document.getElementById(embed_container_id);
     parent.classList.remove("collapse");
 
+    // Always showing the content of the expanded embedded data.
     var embed_content_id = "embed_" + id;
     var elem = document.getElementById(embed_content_id);
     elem.style.display = "contents";
 }
 
 function collapsible_toggle(id) {
-    // Adding hash to url.
+    // Adding or removing hash to/from url.
     toggle_hash_to_url(id)
 
     var embed_container_id = "embed_container_" + id
@@ -108,7 +115,11 @@ function collapsible_toggle(id) {
     elem.style.display = (elem.style.display == "none" ? visible_display : "none");
 };
 
-function collapsible_summary(classname) {
+function collapsible_summary(classname, onload = false) {
+    if (!onload) {
+        toggle_hash_to_url("summary")
+    }
+
     var elem = document.getElementsByClassName(classname);
     var visible_display = "";
     for (var i = 0; i < elem.length; i++) {
@@ -170,7 +181,7 @@ function toggle_contrast_for(target_class) {
 
 function toggle_contrast(onload = false) {
     if (!onload) {
-        toggle_hash_to_url("HC")
+        toggle_hash_to_url("high_contrast")
     }
 
     var step_status_items = document.getElementsByClassName("step-status");
